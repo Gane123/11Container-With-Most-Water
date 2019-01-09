5854a09bbf9940d6f34761556805f139de2afd3e
Gane123 <33416277+Gane123@users.noreply.github.com>
2019-01-09 15:17:32 +0800

    Update code.py

--- 11Container-With-Most-Water/code.py
+++ 11Container-With-Most-Water/code.py
@@ -1,1 +1,20 @@
 
+class Solution:
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        number=int(input(''))
+        height=[int(n) for n in number.split()]
+          height=[random.randint(1,20) for a in range(10)]
+        """
+    
+        S_array=[]
+        for i in height:
+            if i<height[-1]:
+                s=i*(len(height)-height.index(i)-1)
+                S_array.append(s)
+            else:
+                s=height[-1]*(len(height)-height.index(i)-1)
+                S_array.append(s)
+        print(max(S_array))
