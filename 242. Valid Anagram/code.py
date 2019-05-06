f54eaf8a20f1d5483b379c2160d5fb788cbcee67
Gane123 <33416277+Gane123@users.noreply.github.com>
2019-05-06 20:36:00 +0800

    Update code.py

--- 242. Valid Anagram/code.py
+++ 242. Valid Anagram/code.py
@@ -1,3 +1,4 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return sorted(s)==sorted(t)
+        '''return "".join((lambda x:(x.sort(),x)[1])(list(s)))=="".join((lambda x:(x.sort(),x)[1])(list(t)))'''
